a779bd435f6dc7b14ac3cc212f68b1282bad9cd6
maicoljaramillo <micjaramillo@misena.edu.co>
2021-05-08 08:07:12 -0500

    reps

---
+++ javaAdsi/clase1/js/funcion.js
@@ -0,0 +1,44 @@
+
+console.log("HOLA DEDE EL OTRO LADO")
+let saludo="Hola mundo"
+console.log(saludo)
+let dia="7"
+console.log(dia)
+let dia1=7
+console.log(dia)
+function nombreFuncion(){
+   
+}
+function funcionSaludo(){
+    let saludo="Hola desde la funcion"
+    console.log(saludo)
+    document.getElementById("ver").innerHTML = saludo
+}
+function funcionInf(){
+    let nombre="MI NOMBRE ES : MAICOL"
+    let edad="MI EDAD ES : 19 AÑOS"
+    let direc="MI DIRECCION ES : CRA8a 22-60 - Chinchina"
+    let cel="MI CELULAR ES : 3222723505"
+    console.log(nombre)
+    console.log(edad)
+    console.log(direc)
+    console.log(cel)
+    document.getElementById("inf").innerHTML = nombre +  edad + direc +  cel
+}
+function funcionMascota(){
+    let nom="EL NOMBRE DE MI PERRO ES: TERRY"
+    let color="EL COLOR DE TERRY ES : CAFE"
+    let raza="LA RAZA DE TERRY ES : PINCHER"
+    console.log(nom)
+    console.log(color)
+    console.log(raza)
+    document.getElementById("mas").innerHTML = nom+color+raza
+}
+function funcionSerie(){
+    let nomserie=" NOMBRE DE SERIE: BARBAROS"
+    let temserie="TEMPORADAS:1"
+    console.log(nomserie)
+    console.log(temserie)
+    document.getElementById("ser").innerHTML = nomserie+temserie
+}
+/*hacer funciones de mi infopersonal */
